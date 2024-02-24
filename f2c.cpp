5fe7581e7af1d58a0e106444c2322135b70cf28b
Alpiinoo <99215783+Alpiinoo@users.noreply.github.com>
2024-02-24 17:35:50 +0300

    fahrenheit to celsius

---
+++ f2c.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+float celcius(int f) {
+    float c = (5.0 / 9.0) * (f - 32); 
+    return c;
+}
+
+int main() {
+    for (float fah = 20; fah < 71; fah++) {
+     float c = celcius(fah);
+     cout << "F: " << fah <<"\t"<<"C: " << c << "\n";
+    }
+
+    _getch();
+    return 0;
+}
